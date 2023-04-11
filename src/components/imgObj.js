51e625035d93d927091548f69d2e8dffd3f80b1b
DavidMedrano68 <105568134+DavidMedrano68@users.noreply.github.com>
2023-04-10 23:13:02 -0500

    added images

--- src/components/imgObj.js
+++ src/components/imgObj.js
@@ -1,62 +1,63 @@
+import dungDefender from "/src/images/dungDefender.png";
 export const gameCards = [
   {
     id: 1,
     name: "Ghost",
-    url: "src/images/ghost.png",
+    url: "/src/images/ghost.png",
   },
   {
     id: 2,
     name: "Cornifer",
-    url: "src/images/cornifer.png",
+    url: "/src/images/cornifer.png",
   },
   {
     id: 3,
     name: "Hornet",
-    url: "src/images/hornet.png",
+    url: "/src/images/hornet.png",
   },
   {
     id: 4,
     name: "Mantis Lords",
-    url: "src/images/mantisLords.png",
+    url: "/src/images/mantisLords.png",
   },
   {
     id: 5,
     name: "Quirrel",
-    url: "src/images/quirrel.png",
+    url: "/src/images/quirrel.png",
   },
   {
     id: 6,
     name: "Radiance",
-    url: "src/images/radiance.png",
+    url: "/src/images/radiance.png",
   },
   {
     id: 7,
     name: "Soul Master",
-    url: "src/images/soulMaster.png",
+    url: "/src/images/soulMaster.png",
   },
   {
     id: 8,
     name: "Zote",
-    url: "src/images/zote.png",
+    url: "/src/images/zote.png",
   },
   {
     id: 9,
     name: "Watcher Knight",
-    url: "src/images/watcherKnight.png",
+    url: "/src/images/watcherKnight.png",
   },
   {
     id: 10,
     name: "Dung Defender",
-    url: "src/images/dungDefender.png",
+    url: dungDefender,
   },
   {
     id: 11,
     name: "Sly",
-    url: "src/images/sly.png",
+    url: "/src/images/sly.png",
   },
   {
     id: 12,
     name: "Broken Vessel",
-    url: "src/images/brokenVessel.png",
+    url: "/src/images/brokenVessel.png",
   },
 ];
